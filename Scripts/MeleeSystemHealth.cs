8804845035358c330d22adf7c60d4c54e7e4eeae
Dan Ward <dan.steve@hotmail.co.uk>
2016-07-30 02:30:01 +0100

    Generic script folder
    For more information, look into individual scripts.

---
+++ Scripts/MeleeSystemHealth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeSystemHealth : MonoBehaviour {
+
+	public int Health = 100;
+
+	void Update() {
+		if (Health <= 0) {
+			Dead ();
+		}
+	}
+
+	void Dead() {
+		Destroy(gameObject);
+	}
+
+	void ApplyDamage (int damage) {
+		Health -= damage;
+	}
+}
