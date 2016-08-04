52aef9c309cf4eae44ee8b3f829caf02165041aa
Dan Ward <dan.steve@hotmail.co.uk>
2016-08-04 08:13:37 +0100

    ApplyDamage added

--- Scripts/Health.cs
+++ Scripts/Health.cs
@@ -1,15 +1,26 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Health : MonoBehaviour {
 
-    public int health;
+    public float health;
 
     void Update()
     {
         if(health <= 0)
         {
             print("You are dead.");
+            Dead();
         }
+    }
+
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+
+    void ApplyDamage(int damage)
+    {
+        health -= damage;
     }
 }
