76f3b9e2f3d217c8431768712d3aaf82af7ada69
Dan Ward <dan.steve@hotmail.co.uk>
2016-08-04 07:46:00 +0100

    Update EnemyAi.cs

--- Scripts/EnemyAi.cs
+++ Scripts/EnemyAi.cs
@@ -5,28 +5,26 @@ public class EnemyAi : MonoBehaviour {
 
     public float aggroDistance = 10;
     public float meleeDistance = 2;
-    public int attackPower = 10;
+    public float attackPower = 10;
     public float speed;
     private GameObject[] players = new GameObject[8];
     private GameObject target;
+    private Health targetHealthScript;
 
 	void Update () {
         players = GameObject.FindGameObjectsWithTag("Player");
         target = getTarget();
         if(Vector3.Distance(transform.position, target.transform.position) < aggroDistance && target != null)
         {
+            transform.LookAt(target.transform);
             move();
         }
 
         if(Vector3.Distance(transform.position, target.transform.position) < meleeDistance && target != null)
         {
-            RaycastHit hit = new RaycastHit();
-
-            if (Physics.Raycast(transform.position, target.transform.position, out hit))
-            {
-                hit.transform.SendMessage(("ApplyDamage"), attackPower, SendMessageOptions.DontRequireReceiver);
-                print("1");
-            }
+            transform.LookAt(target.transform);
+            targetHealthScript = target.GetComponent<Health>();
+            targetHealthScript.health -= attackPower * Time.deltaTime;
         }
 	}
 
