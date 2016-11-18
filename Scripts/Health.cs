c304416cdc89898ca498bf299e79a67dee1f7f2b
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-18 20:36:27 +0000

    Health

---
+++ Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    public float health;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (health <= 0)
+        {
+            print("You are dead.");
+            Dead();
+        }
+
+    }
+
+    void ApplyDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+
+}
