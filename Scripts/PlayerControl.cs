f55c53c8365860fc2af1b3dd10b2610aa5d93aab
Dan Ward <dan.steve@hotmail.co.uk>
2016-12-06 14:26:19 +0000

    Added other buttons with nice template

--- Scripts/PlayerControl.cs
+++ Scripts/PlayerControl.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
-    public float health;
+    //3d stuff
+    public Transform cam;
 
     //Mouse handling variavles
     private float yRotation;
@@ -14,57 +15,47 @@ public class PlayerControl : MonoBehaviour {
     private float yRotationV;
     private float xRotationV;
     public float lookSmoothnes = 0.1f;
+    public float bottom = 60F;
+    public float top = -60f;
 
     //Movement
     public float speed = 6.0F;
-    private float tempSpeed;
     public float jumpSpeed = 8.0F;
-    private float tempJump;
     public float gravity = 20.0F;
-    private float tempGrav;
     private Vector3 moveDirection = Vector3.zero;
-    delegate void movementChange();
-    delegate void movementStatus();
     private RaycastHit hit;
     public string floor;
 
-    //Lava Variables
-    public int lavaDamage;
+    //animstuff
+    public Animator anim;
+    private float attackTimeCD = 0f;
+
+    //Stat stuff
+    private Statistics stats;
+    private Inventory inv;
+
+    //Audio stuff
+    public AudioSource audioPlayer;
+    public AudioClip weaponAttack;
 
     // Use this for initialization
     void Start () {
-
-        
-
+        Animator[] anims = gameObject.GetComponentsInChildren<Animator>();
+        foreach(Animator a in anims)
+        {
+            if(a.gameObject.name == "PlayerWeapon")
+            {
+                anim = a;
+            }
+        }
+        stats = GetComponent<Statistics>();
+        inv = GetComponent<Inventory>();
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        //floorType status effects
-        Physics.Raycast(transform.position, Vector3.down, out hit, 20.0f);
-        floor = hit.collider.name;
-        movementChange MovementChange;
-        movementStatus MovementStatus;
-
-        switch (floor)
-        {
-            case "Lava":
-                MovementChange = LavaGround;
-                MovementStatus = LavaGroundStatus;
-                break;
-            case "Water":
-                MovementChange = WaterGround;
-                MovementStatus = WaterGroundStatus;
-                break;
-            default:
-                MovementChange = Ground;
-                MovementStatus = GroundStatus;
-                break;
-        }
-
-        MovementChange();
-        MovementStatus();
+        Cursor.lockState = CursorLockMode.Locked;
 
         //Mouse handler
         yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
@@ -72,7 +63,20 @@ public class PlayerControl : MonoBehaviour {
         xRotation = Mathf.Clamp(xRotation, -80, 100);
         currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothnes);
         currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothnes);
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        if(currentXRotation > bottom)
+        {
+            currentXRotation = bottom;
+            xRotation = bottom;
+        }
+
+        if (currentXRotation < top)
+        {
+            currentXRotation = top;
+            xRotation = top;
+        }
+
+        transform.rotation = Quaternion.Euler(0, currentYRotation, 0);
+        cam.transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
 
         //MovementHandler
         CharacterController controller = GetComponent<CharacterController>();
@@ -83,66 +87,62 @@ public class PlayerControl : MonoBehaviour {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             //Multiply it by speed.
-            moveDirection *= tempSpeed;
+            moveDirection *= speed;
             //Jumping
             if (Input.GetButton("Jump"))
-                moveDirection.y = tempJump;
+                moveDirection.y = jumpSpeed;
 
         }
         //Applying gravity to the controller
-        moveDirection.y -= tempGrav * Time.deltaTime;
+        moveDirection.y -= gravity * Time.deltaTime;
         //Making the character move
         controller.Move(moveDirection * Time.deltaTime);
 
-        if (health <= 0)
+
+        //Attack 
+        if (Input.GetMouseButtonDown(0))
         {
-            print("You are dead.");
-            Dead();
+            if (attackTimeCD <= 0)
+            {
+                attackTimeCD = stats.attackTime;
+                anim.SetTrigger("swing");
+                audioPlayer.PlayOneShot(weaponAttack);
+                RaycastHit melee = new RaycastHit();
+                if (Physics.Raycast(transform.position, transform.forward, out melee, stats.meleeReach))
+                {
+                    Debug.DrawLine(transform.position, melee.transform.position, Color.cyan, 10f);
+                    float[] v = new float[6];
+                    v[0] = transform.eulerAngles.y;
+                    v[1] = stats.attack;
+                    v[2] = transform.position.x;
+                    v[3] = transform.position.y;
+                    v[4] = transform.position.z;
+                    melee.transform.SendMessage(("takeDamage"), v, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+            else if (attackTimeCD < 0)
+            {
+                //Do nothing, do time trigger for health stuff before here
+            }
+            
+        }
+        if (attackTimeCD > 0)
+        {
+            attackTimeCD -= Time.deltaTime;
         }
 
-    }
+        //use 
+        if (Input.GetKeyDown("e"))
+        {
+            
+            RaycastHit use = new RaycastHit();
 
-    void Dead()
-    {
-        Destroy(gameObject);
-    }
-
-    void ApplyDamage(int damage)
-    {
-        health -= damage;
-    }
-
-    void LavaGround()
-    {
-        this.tempGrav = gravity * 1;
-        this.tempJump = jumpSpeed * 1;
-        this.tempSpeed = speed * 1;
-    }
-
-    void LavaGroundStatus()
-    {
-        health -= lavaDamage * Time.deltaTime;
-    }
-    void WaterGround()
-    {
-        this.tempGrav = gravity * 0.5f;
-        this.tempJump = jumpSpeed * 0.5f;
-        this.tempSpeed = speed * 0.75f;
-    }
-
-    void WaterGroundStatus()
-    {
-
-    }
-    void Ground()
-    {
-        this.tempGrav = gravity * 1;
-        this.tempJump = jumpSpeed * 1;
-        this.tempSpeed = speed * 1;
-    }
-
-    void GroundStatus()
-    {
-
+            if(Physics.Raycast(transform.position, transform.forward, out use, 2f))
+            {
+                Debug.DrawLine(transform.position, use.transform.position, Color.cyan, 10f);
+                use.transform.SendMessage(("worldUse"), inv, SendMessageOptions.DontRequireReceiver);
+            }
+       }
+       
     }
 }
